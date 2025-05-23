8c53a46e9fa099cca278d1e5ea0a359548ece9ca
niikun <123465566+niikun@users.noreply.github.com>
2025-05-23 16:22:54 +0000

    blackを適用

--- backend/app/chat_bot.py
+++ backend/app/chat_bot.py
@@ -1,13 +1,16 @@
+"""ChatGPTを使用して質問に答えるためのコード"""
+
 import os
 from langchain_openai import ChatOpenAI
 
 model = ChatOpenAI(
     model_name="gpt-4o-mini",
     temperature=0.0,
-    openai_api_key=os.getenv("OPENAI_API_KEY")
+    openai_api_key=os.getenv("OPENAI_API_KEY"),
 )
 
-def create_response(query: str) :
+
+def create_response(query: str):
     """クエリを受け取り、モデルに渡して応答を取得する"""
     query = query.strip()
     if not query:
